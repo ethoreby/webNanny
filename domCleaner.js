d2e32efc03fae7d3242c08fd5ed33e21f485d6d2
Hack Reactor Students <accounts@hackreactor.com>
2014-03-31 12:23:20 -0700

    functional word replacement test

--- domCleaner.js
+++ domCleaner.js
@@ -1,2 +1,23 @@
-alert("this actually worked");
-alert($("h1").text());
+$(function() {
+  alert("running dom cleaner");
+
+  var dictionary = {
+    "mariner": "REPLACED!",
+    "mariners": "REPLACED!"
+  };
+
+  $("h1").each(function() {
+    if($(this).text) {            //check if the current element has text
+
+      var words = $(this).text().split(" ");
+      for(var i = 0; i < words.length; i++) {
+        var word = words[i];
+        if(dictionary[word.toLowerCase()]) {
+          words[i] = dictionary[word.toLowerCase()];
+        }
+      }
+      $(this).text(words.join(" "));
+    }
+  });
+
+});
