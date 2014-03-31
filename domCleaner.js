29d0c25173a4dd7b7a27cd3a2044f814c2c89e8e
Hack Reactor Students <accounts@hackreactor.com>
2014-03-31 16:36:28 -0700

    added profanity regex dictionary

--- domCleaner.js
+++ domCleaner.js
@@ -1,9 +1,26 @@
 $(function() {
   alert("running dom cleaner");
 
-  var parseElements = function(el) {
+  var dictionary = {
+    "gosh darn": /Goddamn|God damn/gi,
+    "dang": /damn/gi,
+    "flip": /feck|fuck/gi,
+    "heck": /hell/gi,
+    "poo": /shit|turd/gi,
+    " poo ": / crap /gi,
+    "friend": /nigger|nigga|pimp/gi,
+    "guy": /coon|dyke|faggot|fag|fudgepacker|fudge packer|homo|queer/gi,
+    "sleepover": /blowjob|blow job|fellate|fellatio|orgasm|sex|wank/gi,
+    "dummy": /asshole|bitch|biatch|bastard|cocksucker|cunt|jackoff|jack off|jerkoff|scumbag|skank|slut|tosser|twat|whore/gi,
+    "bum": /anal|anus|arse|butt|butthole|rectum/gi,
+    "bum": / ass /gi,
+    "privates": /ballsack|balls|boner|boob|clit|clitoris|cock|dick|dildo|labia|muff|pecker|penis|prick|pube|pussy|schlong|scrotum|tit|vagina/gi,
+    "yuck": /jizz|piss|smegma|spunk/gi,
+    " yuck ": / cum /gi,
+  };
 
-    if(el.nodeType === 3) {
+  var parseElements = function(el) {
+    if(el.nodeType === 3) {       // 3 = text node
       purgeElement(el);
     }else {
       for(var i = 0; i < el.childNodes.length; i++) {
@@ -13,14 +30,15 @@ $(function() {
   };
 
   var purgeElement = function(el) {
-    var re = /mariner/gi;
     var str = el.data;
-    var str = str.replace(re, "seahawk");
+    for(var key in dictionary) {
+      str = str.replace(dictionary[key], key);
+    }
 
     el.data = str;
   };
 
-  $("p").each(function() {
+  $("body").each(function() {
     parseElements(this);
   });
 });
