7ae40d2bd482dee14e412e25ec1357088c2e125c
Hack Reactor Students <accounts@hackreactor.com>
2014-03-31 22:39:03 -0700

    error and success messaging for input definitions

--- run.js
+++ run.js
@@ -3,12 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('.clean').addEventListener('click', function() {
     chrome.tabs.executeScript(null, { file: "jquery.min.js" }, function() {
       chrome.tabs.executeScript(null, { file: "domCleaner.js" }, function(results) {
-        $(".cleanPage").append("<div class='finished'> WebNanny removed " + results[0] + " words from this page.</div>");
+        $(".alerts").empty();
+        $(".alerts").append("<div class='finished'> WebNanny removed " + results[0] + " words from this page.</div>");
       });
     });
   });
 
   $(".newDefinition").on("click", function() {
+    var word = $(".ipWord").val();
+    var replacement = $(".ipReplacement").val();
 
+    $(".alerts").empty();
+
+    if(word.length === 0 || replacement.length === 0) {
+      $(".alerts").append("<div class='error'>Oops! Please enter a word and a replacement definition.</div>");
+    }else {
+      $(".alerts").append("<div class='finished'>Definition added: replace " + word + " with " + replacement + "</div>");
+    }
   });
 });
